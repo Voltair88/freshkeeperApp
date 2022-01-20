46d0bb4ced29825669420087b9ebe2090156365a
Voltair88 <70101120+Voltair88@users.noreply.github.com>
2022-01-20 16:56:24 +0100

    init commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,63 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
-export default function App() {
+
+type RootStackParamList = {
+  Home: undefined, // undefined because you aren't passing any params to the home screen
+  Details: undefined; 
+};
+
+type ProfileScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
+
+type Props = {
+  navigation: ProfileScreenNavigationProp;
+};
+
+function HomeScreen({ navigation }: Props) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
+
+function DetailsScreen({ navigation }: Props) {
+  return (
+    <View style={styles.container}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Home Screen"
+        onPress={() => navigation.push('Home')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
