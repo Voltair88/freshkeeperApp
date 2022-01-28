bea5cb5605f7afe81a472860a6dab089be27e2ea
Voltair88 <Voltair88@gmail.com>
2022-01-28 15:54:05 +0100

    added spinner and titles

--- src/screens/Additem.tsx
+++ src/screens/Additem.tsx
@@ -8,6 +8,10 @@ import { useForm, Controller } from "react-hook-form";
 import InputSpinner from "react-native-input-spinner";
 
 function Additem({ navigation }: NativeStackScreenProps<RootTabParamList>) {
+  /*   function setState(arg0: { value: number }): void {
+    throw new Error("Function not implemented.");
+  }
+   */
   const {
     control,
     handleSubmit,
@@ -25,6 +29,11 @@ function Additem({ navigation }: NativeStackScreenProps<RootTabParamList>) {
       <Text style={styles.tabsubtitle}>
         choose your product, storage and expiration date.
       </Text>
+      <Text style={styles.devider} />
+      <View style={styles.flexrow}>
+        <Text style={styles.itemnumber}>1</Text>
+        <Text style={styles.itemname}>Add product name</Text>
+      </View>
       <Controller
         control={control}
         rules={{
@@ -32,7 +41,7 @@ function Additem({ navigation }: NativeStackScreenProps<RootTabParamList>) {
         }}
         render={({ field: { onChange, onBlur, value } }) => (
           <TextInput
-            style={styles.input}
+            style={styles.nameinput}
             onBlur={onBlur}
             onChangeText={onChange}
             value={value}
@@ -40,31 +49,52 @@ function Additem({ navigation }: NativeStackScreenProps<RootTabParamList>) {
         )}
         name="name"
       />
+      <Text style={styles.devider} />
+      <View style={styles.flexrow}>
+        <Text style={styles.itemnumber}>2</Text>
+        <Text style={styles.itemname}> Choose quantity</Text>
+      </View>
       {errors.name && <Text>This is required.</Text>}
       <Controller
         control={control}
         rules={{
           maxLength: 100,
+          required: true,
         }}
         render={({ field: { onChange, value } }) => (
           <InputSpinner
+            style={styles.numberinput}
             max={100}
             min={0}
             step={1}
-            colorMax={"#f04048"}
-            colorMin={"#40c5f4"}
+            buttonStyle={{
+              backgroundColor: "white",
+              borderColor: "#49BEFF",
+              borderWidth: 2,
+            }}
+            inputStyle={{
+              backgroundColor: "white",
+              borderColor: "#49BEFF",
+              borderWidth: 2,
+              borderRadius: 70 / 2,
+              height: 70,
+            }}
+            fontSize={40}
+            prepend={<View style={{ width: 30 }} />}
+            append={<View style={{ width: 30 }} />}
+            buttonTextColor="black"
+            /*             colorMax={"#f04048"}
+            colorMin={"#40c5f4"} */
             value={value}
             onChange={(number) => onChange(number)}
           />
         )}
         name="number"
       />
+      <Text style={styles.devider} />
 
       <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
   );
 }
 export default Additem;
-function setState(arg0: { value: number }): void {
-  throw new Error("Function not implemented.");
-}
--- styles.tsx
+++ styles.tsx
@@ -1,13 +1,11 @@
 import { StyleSheet } from "react-native";
 import Constants from "expo-constants";
-
+import { Dimensions } from "react-native";
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     paddingTop: Constants.statusBarHeight,
     padding: 8,
-    backgroundColor: "#0e101c",
   },
   header: {
     flex: 1,
@@ -24,21 +22,17 @@ const styles = StyleSheet.create({
   tabtitle: {
     fontFamily: "Inter_700Bold",
     fontSize: 26,
-    marginHorizontal: "auto",
     textAlign: "center",
-    borderBottomWidth: 1.5,
     marginLeft: 20,
     marginRight: 20,
-    paddingVertical: 20,
-    borderBottomColor: "#6363637f",
+    paddingVertical: 5,
   },
   tabsubtitle: {
+    borderBottomColor: "#6363637f",
     fontFamily: "Inter_300Light",
     fontSize: 12,
     fontStyle: "italic",
-    marginHorizontal: "auto",
     textAlign: "center",
-    paddingVertical: 20,
   },
   body: {
     flex: 10,
@@ -46,11 +40,51 @@ const styles = StyleSheet.create({
   navbar: {
     flex: 1,
   },
-  input: {
+  nameinput: {
     backgroundColor: "white",
     height: 40,
-    padding: 10,
+    paddingHorizontal: 10,
+    marginHorizontal: 40,
+    marginVertical: 10,
     borderRadius: 4,
+  },
+  numberinput: {
+    height: 70,
+    paddingHorizontal: 90,
+    borderRadius: 4,
+    marginVertical: 10,
+    borderColor: "#49BEFF",
+  },
+  devider: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#6363637f",
+    marginTop: 10,
+    height: 1,
+    marginHorizontal: 20,
+  },
+  flexrow: {
+    flexDirection: "row",
+    /*     alignItems: "center", */
+  },
+  itemname: {
+    fontFamily: "Inter_700Bold",
+    fontSize: 16,
+    marginTop: 10,
+    left: Dimensions.get("window").width * 0.25,
+    width: Dimensions.get("window").width * 0.5,
+    right: Dimensions.get("window").width * 0.25,
+  },
+  itemnumber: {
+    fontFamily: "Inter_700Bold",
+    backgroundColor: "#49BEFF",
+    borderRadius: 30 / 2,
+    color: "white",
+    width: 30,
+    height: 30,
+    textAlign: "center",
+    paddingVertical: 5,
+    left: 10,
+    marginTop: 10,
   },
 });
 
