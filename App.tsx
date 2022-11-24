d9880f04302aa48189f7b233ef6a594a7c11d846
Voltair88 <Voltair88@gmail.com>
2022-11-24 10:32:12 +0100

    reinstalled expo-app with typescript template

--- App.tsx
+++ App.tsx
@@ -1,31 +1,22 @@
-import * as React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import styles from "./styles";
-import { Text, View } from "react-native";
-import AppLoading from "expo-app-loading";
-import { useFonts, Pacifico_400Regular } from "@expo-google-fonts/pacifico";
-import { Roboto_400Regular } from "@expo-google-fonts/roboto";
-import { Inter_300Light, Inter_700Bold } from "@expo-google-fonts/inter";
-import { Header, Navbar } from "./src/components/Component.index";
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-function App() {
-  let [fontsLoaded] = useFonts({
-    Pacifico_400Regular,
-    Roboto_400Regular,
-    Inter_300Light,
-    Inter_700Bold,
-  });
-  if (!fontsLoaded) {
-    return <AppLoading />;
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
   } else {
     return (
-      <NavigationContainer>
-        <Header />
-        <View style={styles.body}>
-          <Navbar />
-        </View>
-      </NavigationContainer>
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
     );
   }
 }
-export default App;
